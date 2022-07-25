97c77e6f1149bb25917a7e39cda05c7ca3e3e58e
priess0 <honza.svejdik@gmail.com>
2022-07-25 10:45:37 +0200

    Created a single random RPS generator

---
+++ rps.js
@@ -0,0 +1,19 @@
+const choice;
+var game = Math.floor(Math.random() * 3) + 1;
+console.log(game);
+if game === 1 {
+  const choice === "rock";
+} else if {
+  const choice === "paper";
+} else {
+  const choice === "scissors";
+}
+
+
+    // set constant for rock
+    // set constant for paper
+    // set constant for scissors
+    // set variable for game
+
+    // have game use math.random to pick a number
+    // assign
